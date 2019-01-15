3e3c74714beadbccd5519410069cce6f2f32779a
vincentdu <vdu@scilearn.com>
2019-01-14 18:03:43 -0800

    adding in remaining convsation dialogue to database

--- db/seeds.rb
+++ db/seeds.rb
@@ -253,13 +253,251 @@ VC1S06A = Choice.create(
     game: game    
 )
 
-greet_again = Choice.create(
-    text: "Thank you sire.", 
+greetings_again = Choice.create(
+    text: "Greetings again sire. Have a good day.", 
     choice_type: "CLOSE",
     game: game    
 )
 
-VC1S012.outcomes.create([{choice: VC1S06A}, {choice: greet_again}])
+VC1S012.outcomes.create([{choice: VC1S06A}, {choice: greetings_again}])
+
+VC1S013 = Contact.create(
+    text: "Greetings Warrior. I am Illugi the lead worker of this worker unit. How may I help you?",
+    character_type: "Illugi",
+    hierarchy: 1,
+    game: game
+)
+
+VC1S07A = Choice.create(
+    text: "Greetings sir, can you tell me if you've seen any glowing stones?",
+    choice_type: "NORMAL",
+    game: game    
+)
+
+honor = Choice.create(
+    text: "It is an honor to meet you sir, good day.",
+    choice_type: "CLOSE",
+    game: game    
+)
+
+VC1S013.outcomes.create([{choice: VC1S07A}, {choice: honor}])
+
+VC1S014 = Contact.create(
+    text: "Yes Warrior, my men had one found one lying on top of the ground the other day. Unfortunately they had pile some wood on top of it so if you like it you will need an axe to chop through the wood to get it.",
+    character_type: "Illugi",
+    hierarchy: 2,
+    game: game
+)
+
+VC1S08A = Choice.create(
+    text: "I see, can you tell me where I can get an axe?",
+    choice_type: "NORMAL",
+    game: game    
+)
+
+honor_and_info = Choice.create(
+    text: "It is an honor to meet you sir, thank you for the info good day.",
+    choice_type: "CLOSE",
+    game: game    
+)
+
+VC1S014.outcomes.create([{choice: VC1S08A}, {choice: honor_and_info}])
+
+VC1S015 = Contact.create(
+    text: "You can ask our Earl Erik the Red, he may be able to give you one if you deliver a message for him.",
+    character_type: "Illugi",
+    hierarchy: 2,
+    game: game
+)
+
+VC1S015.outcomes.create([{choice: honor_and_info}])
+
+VC1S015 = Contact.create(
+    text: "Greetings again Warrior.",
+    character_type: "Illugi",
+    hierarchy: 2,
+    game: game
+)
+
+VC1S09A = Choice.create(
+    text: "Greetings again sir, can you tell me again if you've seen any glowing stones?",
+    choice_type: "NORMAL",
+    game: game    
+)
+
+VC1S010A = Choice.create(
+    text: "Greetings again sir, can you tell me again where I can get an axe?",
+    choice_type: "NORMAL",
+    game: game    
+)
+
+VC1S015.outcomes.create([{choice: VC1S09A}, {choice: VC1S010A}, {choice: honor_and_info}])
+
+VC1S016 = Contact.create(
+    text: "Yes Warrior, my men had one found one lying on top of the ground the other day. Unfortunately they had pile some wood on top of it so if you like it you will need an axe to chop through the wood to get it.",
+    character_type: "Illugi",
+    hierarchy: 2,
+    game: game
+)
+
+VC1S011A = Choice.create(
+    text: "Greetings again sir, can you tell me again where I can get an axe?",
+    choice_type: "NORMAL",
+    game: game    
+)
+
+VC1S016.outcomes.create([{choice: VC1S011A}, {choice: honor_and_info}])
+
+VC1S017 = Contact.create(
+    text: "Greetings Warrior, how may I be of assistance",
+    character_type: "ShipCaptain",
+    hierarchy: 1,
+    game: game
+)
+
+VC1S012A = Choice.create(
+    text: "Greetings sir, I have a message for you from our Earl Erik the Red.",
+    choice_type: "NORMAL",
+    game: game    
+)
+
+VC1S013A = Choice.create(
+    text: "Greetings sir, have you seen any glowing orbs today?",
+    choice_type: "NORMAL",
+    game: game    
+)
+
+nice_to_meet = Choice.create(
+    text: "It is nice to meet you sir, have a good day.",
+    choice_type: "CLOSE",
+    game: game    
+)
+
+VC1S017.outcomes.create([{choice: VC1S012A}, {choice: VC1S013A}, {choice: nice_to_meet}])
+
+VC1S018 = Contact.create(
+    text: "Thank you warrior, please report back to the Earl that I have received it. How else can I be of assistance?",
+    character_type: "ShipCaptain",
+    hierarchy: 2,
+    game: game
+)
+
+VC1S014A = Choice.create(
+    text: "Yes sir one question, have you seen any glowing orbs today?",
+    choice_type: "NORMAL",
+    game: game    
+)
+
+good_day = Choice.create(
+    text: "That is all sir thank you, have a good day.",
+    choice_type: "CLOSE",
+    game: game    
+)
+
+VC1S018.outcomes.create([{choice: VC1S014A}, {choice: good_day}])
+
+VC1S019 = Contact.create(
+    text: "Yes I saw one near the edge of the river South of here. However, you probably need a net to pick it up from the water. You can grab one from Glaumur. He is in charge of supplies near the village center.",
+    character_type: "ShipCaptain",
+    hierarchy: 2,
+    game: game
+)
+
+thanks_good_day = Choice.create(
+    text: "Thank you again sir, have a good day.",
+    choice_type: "CLOSE",
+    game: game    
+)
+
+VC1S019.outcomes.create([{choice: thanks_good_day}])
+
+VC1S020 = Contact.create(
+    text: "Yes I saw one near the edge of the river South of here. However, you probably need a net to pick it up from the water. You can grab one from Glaumur. He is in charge of supplies near the village center.",
+    character_type: "ShipCaptain",
+    hierarchy: 2,
+    game: game
+)
+
+VC1S015A = Choice.create(
+    text: "Thank you sir, I also have a message for you from our Earl Erik the Red.",
+    choice_type: "NORMAL",
+    game: game    
+)
+
+VC1S020.outcomes.create([{choice: VC1S015A}, {choice: thanks_good_day}])
+
+VC1S021 = Contact.create(
+    text: "Greetings again Warrior, how may I be of assistance",
+    character_type: "ShipCaptain",
+    hierarchy: 2,
+    game: game
+)
+
+VC1S016A = Choice.create(
+    text: "Greetings again sir, I have a message for you from our Earl Erik the Red.",
+    choice_type: "NORMAL",
+    game: game    
+)
+
+VC1S017A = Choice.create(
+    text: "Greetings again sir, have you seen any glowing orbs today?",
+    choice_type: "NORMAL",
+    game: game    
+)
+
+VC1S021.outcomes.create([{choice: VC1S016A}, {choice: VC1S017A}, {choice: thanks_good_day}])
+
+VC1S022= Contact.create(
+    text: "Greetings Warrior, how may I be of assistance",
+    character_type: "Glaumur",
+    hierarchy: 1,
+    game: game
+)
+
+VC1S017A = Choice.create(
+    text: "Greetings sir, may I have a net from you.",
+    choice_type: "NORMAL",
+    game: game    
+)
+
+VC1S018A = Choice.create(
+    text: "Greetings sir, have you seen any glowing orbs today?",
+    choice_type: "NORMAL",
+    game: game    
+)
+
+VC1S022.outcomes.create([{choice: VC1S017A}, {choice: VC1S018A}, {choice: thanks_good_day}])
+
+VC1S023= Contact.create(
+    text: "Certainly, here you go warrior.",
+    character_type: "Glaumur",
+    hierarchy: 2,
+    game: game
+)
+
+VC1S019A = Choice.create(
+    text: "Thank you sir, have you seen any glowing orbs today?",
+    choice_type: "NORMAL",
+    game: game    
+)
+
+VC1S023.outcomes.create([{choice: VC1S019A}, {choice: thanks_good_day}])
+
+VC1S024= Contact.create(
+    text: "Yes I have heard that Grettir and his band of traitors has found one to the North of the village near the forests. If you go to get it beware they may be hostile.",
+    character_type: "Glaumur",
+    hierarchy: 2,
+    game: game
+)
+
+VC1S020A = Choice.create(
+    text: "Greetings sir, may I have a net from you.",
+    choice_type: "NORMAL",
+    game: game    
+)
+
+VC1S024.outcomes.create([{choice: VC1S020A}, {choice: thanks_good_day}])
+
 
 binding.pry
 
